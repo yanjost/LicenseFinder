fe9a6bc081576223b59b9d08a82342f9996897c6
Yannick JOST <yannick@scalingo.com>
2021-03-22 17:31:00 +0100

    Fix action

--- action_entrypoint.sh
+++ action_entrypoint.sh
@@ -4,6 +4,10 @@
 # or splits `args` in a string separated by whitespace.
 sh -c "echo $*"
 
-echo "Starting license_finder"
+SCAN_FOLDER=$1
+
+echo "Starting license_finder in folder ${SCAN_FOLDER}"
+
+cd ${SCAN_FOLDER}
 
 license_finder action_items --enabled-package-managers=yarn
